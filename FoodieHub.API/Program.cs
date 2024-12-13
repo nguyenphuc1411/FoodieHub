78782337c670de5806a027cc9fa260e339084007
Phuc Nguyen <nguyenphuc14112003@gmail.com>
2024-12-13 17:44:31 +0700

    update

--- FoodieHub.API/Program.cs
+++ FoodieHub.API/Program.cs
@@ -17,7 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
-if (builder.Environment.IsDevelopment())
+/*if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AppDBDevelopment")));
@@ -26,8 +26,9 @@ else
 {
     builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AppDBProduction")));
-}
-
+}*/
+builder.Services.AddDbContext<AppDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("AppDBProduction")));
 
 // C?u hình Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
